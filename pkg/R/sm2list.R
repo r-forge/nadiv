61cf9eaa6c6d52fccc9c0af348950461f0fdf90d
mwola (Matthew Wolak) <matthewwolak@gmail.com>
2011-11-17 18:59:59 +0000

    made sm2list() for lower triangles (changed > to <)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nadiv@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sm2list.R
+++ pkg/R/sm2list.R
@@ -3,7 +3,7 @@ sm2list<-function (A = NULL, rownames = NULL, colnames=c("row", "column", "A"))
     ginv <- data.frame(Row = rep(1:length(A@p[-1]),
         diff(A@p)), Column = A@i + 1, Ainverse = A@x)
     ginv <- ginv[order(ginv$Row), ]
-    ginv <- ginv[which(ginv$Row >= ginv$Column), ]
+    ginv <- ginv[which(ginv$Row <= ginv$Column), ]
     attr(ginv, "rowNames") <- rownames
     names(ginv)<-colnames
     return(ginv)
