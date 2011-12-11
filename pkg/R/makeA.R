4c062d8a8321bdf80f9fc4a2af9dbdaec1bc6549
mwola (Matthew Wolak) <matthewwolak@gmail.com>
2011-12-11 18:48:21 +0000

    took out inverseA() from MCMCglmm as makeA and now have fortran Meuwissen and Luo algorithm for makeA
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nadiv@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/makeA.R
+++ pkg/R/makeA.R
@@ -1,7 +1,34 @@
 makeA<-function (pedigree) 
 {
-Asparse <- zapsmall(solve(inverseA(pedigree)$Ainv), 10)
-A <- as(Asparse, "matrix")
+  numeric.pedigree <- numPed(pedigree)
+  N <- dim(numeric.pedigree)[1]
+  ped <- t(numeric.pedigree)
+  T <- matrix(0, N, N)
+  F <- rep(0, N)
+  Dii <- rep(0, N)
+
+  out <- .Fortran(AML, ped_n=as.integer(N),
+	ped=as.integer(ped),
+	T=as.single(T),
+	F=as.single(F),
+	Dii=as.single(Dii),
+	Aout=as.single(T))
+
+
+  T.i <- as.integer(rep(1:N, N)[which(out$T > 0)]-1)
+  T.x <- out$T[which(out$T > 0)]
+  T.j <- as.integer(rep(1:N, each=N)[which(out$T>0)]-1)
+  T <- new("dtTMatrix", Dim=as.integer(c(N,N)))
+  T@uplo <- "L"
+  T@i <- T.i
+  T@x <- T.x
+  T@j <- T.j
+
+  D <- Diagonal(N, sqrt(out$Dii))
+  L <- T%*%D
+  Asparse <- L%*%t(L)
+  A <- as(Asparse, "matrix")
+  
 A
 }
 
