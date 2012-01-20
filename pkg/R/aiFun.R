e4c3c936701de9226178041db6fd73df5e083728
mwola (Matthew Wolak) <matthewwolak@gmail.com>
2012-01-20 22:43:19 +0000

    cleaned up a few problems
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nadiv@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aiFun.R
+++ pkg/R/aiFun.R
@@ -8,7 +8,7 @@ aiFun <- function(model = NULL, AI.vec = NULL, inverse = TRUE, Dimnames=NULL)
     if(inverse == FALSE) AI.cov <- solve(AI.cov)    
  
   AI.cor <- cov2cor(AI.cov)
-  AI.mat <- matrix(0, nrow=k, ncol=k)
+  AI.mat <- matrix(0, nrow=dim(AI.cov)[1], ncol=dim(AI.cov)[2])
   AI.mat[upper.tri(AI.cor, diag=FALSE)] <- AI.cor[upper.tri(AI.cor, diag=FALSE)]
   AI.mat[lower.tri(AI.cov, diag=TRUE)] <- AI.cov[lower.tri(AI.cov, diag=TRUE)]
   if(is.null(Dimnames)){Dimnames <- names(model$gammas)}
